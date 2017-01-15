83bb9a8092ef04483723c807699d184db11f9c79
Connor McKay <mckayconnort@gmail.com>
2017-01-15 15:26:24 -0500

    fix tests file

--- tests/test_funcs.R
+++ tests/test_funcs.R
@@ -1,17 +1,14 @@
 # unit tests for bradford repo functions
 library(testthat)
 
-source("bayesian-explorer/functions/bayesian_functions.R")
+source("../functions/bayesian_functions.R")
 
 context("Bayesian Functions")
 test_that("betaPosterior outputs the correct vector", {
-  x <- readRDS(file = "bayesian-explorer/tests/test_data/test_post_df.RDS")
-  expect_equal_to_reference(betaPosterior(x, prior.mean = .5, prior.n = 10, sample.n = "sample.n", affirm.n = "affirm.n"),
-                            "bayesian-explorer/tests/test_data/test_posterior.RDS")
+  expect_equal_to_reference(betaPosterior(prior.mean = .5, prior.n = 10, sample.n = 100, affirm.n = 50), 
+                            "test_data/beta_posterior.RDS")
 })
 
 test_that("betaPosteriorMean outputs the correct scalar", {
-  x <- readRDS(file = "bayesian-explorer/tests/test_data/test_post_df.RDS")
-  expect_equal_to_reference(betaPosteriorMean(x, prior.mean = .5, prior.n = 10, sample.n = "sample.n", affirm.n = "affirm.n"),
-                            "bayesian-explorer/tests/test_data/test_posterior_mean.RDS")
+  expect_equal(betaPosteriorMean(prior.mean = .5, prior.n = 10, sample.n = 100, affirm.n = 50), .5)
 })
