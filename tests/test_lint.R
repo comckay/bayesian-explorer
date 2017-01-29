61124fec3c54f263ae7d854360fbf4242b0e591e
comckay <mckayconnort@gmail.com>
2017-01-29 18:07:22 -0500

    redo travis file and roll back changes for lint testing. This will now be run after a sucessful test of function tests.

--- tests/test_lint.R
+++ tests/test_lint.R
@@ -4,16 +4,6 @@ library(lintr)
 if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("Code is Lint Free", {
-    x <- list.files(path = "../", pattern = ".R$",
-                    full.names = T, recursive = T, include.dirs = T) %>%
-      lapply(function(x) lintr::lint(x)) %>%
-      {
-        lint.count <<- purrr::map(., length)
-        return(.)
-      }
-    if (sum(unlist(x)) > 0) {
-      print(c("Please lint and fix the following files:", unique(x$filename)))
-    }
-    expect_equal(sum(unlist(x)), 0)
+    lintr::expect_lint_free(path = "../")
   })
 }
